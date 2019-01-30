4fedea442f8639ef527f06917ce70c5e254b9903
jitaek <jitaek@Lecturer-PC>
2019-01-30 09:15:19 +0900

    add course class

--- schoolmgr/src/main/java/org/study/mgr/model/Instructor.java
+++ schoolmgr/src/main/java/org/study/mgr/model/Instructor.java
@@ -1,5 +1,8 @@
 package org.study.mgr.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -7,6 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
@@ -34,6 +38,13 @@ public class Instructor {
 	@JoinColumn(name = "instructor_detail_id")
 	private InstructorDetail instructorDetail;
 
+	@OneToMany(mappedBy = "instructor",
+			cascade = {
+					CascadeType.DETACH, CascadeType.MERGE,
+					CascadeType.PERSIST, CascadeType.REFRESH
+			})
+	private List<Course> courses;
+	
 	public Instructor() {
 	}
 
@@ -83,10 +94,29 @@ public class Instructor {
 		this.instructorDetail = instructorDetail;
 	}
 
+	public List<Course> getCourses() {
+		return courses;
+	}
+
+	public void setCourses(List<Course> courses) {
+		this.courses = courses;
+	}
+
 	@Override
 	public String toString() {
 		return "Instructor [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
 				+ ", instructorDetail=" + instructorDetail + "]";
+	}
+	
+	// add convenience methods for bi-directional relationship
+	public void add(Course tempCourse) {
+		if (courses == null) {
+			courses = new ArrayList<> ();
+		}
+		
+		courses.add(tempCourse);
+		
+		tempCourse.setInstructor(this);
 	}
 
 }
